e64bde0d78f1ee95c44e1f07428ba95922235eb9
Symon Cristhian <contatosymon@gmail.com>
2021-03-05 00:48:48 -0400

    Countdown Context and notifications configurations

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,11 +1,23 @@
 import { useContext } from 'react'
 import { ChallengesContext } from '../contexts/ChallengeContext'
+import { CountdownContext } from '../contexts/CountdownContext'
 
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox() {
 
-    const { activeChallenge, resetChallenge } = useContext(ChallengesContext)
+    const { activeChallenge, resetChallenge, completedChallenge } = useContext(ChallengesContext)
+    const { resetCountdown } = useContext(CountdownContext)
+
+    function handleChallengeSucceed() {
+        completedChallenge()
+        resetCountdown()
+    }
+
+    function handleChallengeFailed() {
+        resetChallenge()
+        resetCountdown()
+    }
 
     return (
         <div className={styles.challengeBoxContainer}>
@@ -23,13 +35,14 @@ export function ChallengeBox() {
                         <button
                             type="button"
                             className={styles.challengeSucceededButton}
+                            onClick={handleChallengeSucceed}
                         >
                             Succeeded
                         </button>
                         <button
                             type="button"
                             className={styles.challengeFailedButton}
-                            onClick={resetChallenge}
+                            onClick={handleChallengeFailed}
                         >
                             Failed
                         </button>
@@ -37,7 +50,7 @@ export function ChallengeBox() {
                 </div>
             ) : (
                     <div className={styles.challengeNotActive}>
-                        <strong>Finish a cycle to earn challenges to be completed</strong>
+                        <strong>Finish a cycle to receive challenges to be completed</strong>
                         <p>
                             <img src="icons/level-up.svg" alt="Level Up" />
                     Level up completing challenges
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengeContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/s4imu.png" alt="Symon Cristhian" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Symon Cristhian</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
